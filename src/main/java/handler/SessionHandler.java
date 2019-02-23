5a485e35a18edef0a3d231dc6a644b91437da539
Emilio Barboza <pringles212@gmail.com>
2019-02-23 18:48:09 -0400

    SessionHandler consulta en la DB

--- src/main/java/handler/SessionHandler.java
+++ src/main/java/handler/SessionHandler.java
@@ -5,10 +5,46 @@
  */
 package handler;
 
+import Model.UserModel;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.servlet.http.HttpServletRequest;
+import util.DBConnection;
+import util.Encrypter;
+import util.PropReader;
+import util.SuperMapper;
+
 /**
  *
  * @author Emilio
  */
 public class SessionHandler {
-  
+
+  private DBConnection db;
+  private PropReader prpReader;
+  private SuperMapper jackson;
+  private ResultSet rs;
+
+  public String loginUser(HttpServletRequest request) throws SQLException {
+
+    prpReader = PropReader.getInstance();
+    db = new DBConnection(prpReader.getValue("dbDriver"), prpReader.getValue("dbUrl"), prpReader.getValue("dbUser"), prpReader.getValue("dbPassword"));
+    jackson = new SuperMapper();
+    String resp = "";
+    System.out.print("Response:" + resp);
+    try {
+      UserModel user = jackson.jsonToPlainObj(request, UserModel.class);
+      rs = db.execute(prpReader.getValue("loginUser"), user.getUsername(), Encrypter.getMD5(user.getPassword()));
+      if (rs.next()) {
+        resp = "Ok";
+      } else {
+        resp = "Error";
+      }
+      db.closeCon();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    return resp;
+  }
 }
--- src/main/java/servlets/SessionServlet.java
+++ src/main/java/servlets/SessionServlet.java
@@ -5,7 +5,12 @@
  */
 package servlets;
 
+import handler.SessionHandler;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,18 +23,39 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(name = "SesionServlet", urlPatterns = {"/session"})
 public class SessionServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
 
+  private static final long serialVersionUID = 1L;
+
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    SessionHandler loginUser = new SessionHandler();
+    PrintWriter out = response.getWriter();
+
+    try {
+      String status = loginUser.loginUser(request);
+      switch (status) {
+        case "Ok":
+          out.print(200);
+          response.setStatus(200);
+          break;
+        case "Error":
+          out.print(401);
+          break;
+        default:
+          out.print(500);
+          break;
+      }
+    } catch (SQLException ex) {
+      Logger.getLogger(SessionServlet.class.getName()).log(Level.SEVERE, null, ex);
     }
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-
-    }
-
+  }
 
 }
